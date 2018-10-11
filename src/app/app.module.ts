d09ad62c2886000024c34a581cb03a4db84c1cfa
Daniel Choque <patzidaniel@gmail.com>
2018-10-10 21:05:02 -0500

    agregado dataPicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,15 @@ import { HttpModule } from '@angular/http';
 import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatSortModule} from '@angular/material/sort';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './index/login/login.component';
@@ -27,7 +34,12 @@ import { LoginComponent } from './index/login/login.component';
     MatIconModule,
     MatDatepickerModule,
     MatCheckboxModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatSortModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
