feb2e16681b69ee890f83e3d546f540d46358628
Daniel Choque <patzidaniel@gmail.com>
2018-12-18 16:10:19 -0400

    plaqntilla

--- src/app/model/Network.ts
+++ src/app/model/Network.ts
@@ -1,5 +1,5 @@
 export class Network {
-    public static API_URL = "http://192.168.0.102:8080/";  
+    public static API_URL = "http://192.168.10.254:8080/";  
     //public static API_URL = "http://127.0.0.1:8080/";  
     //public static IMAGE_URL="/src/assets/image";    
     public static IMAGE_URL="assets/image";
