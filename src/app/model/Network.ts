6c2288425f01d04e9bc648dbe5be94ba75f3e88b
gonzalo <meru.vi06@gmail.com>
2018-12-23 18:24:44 -0400

    URL A LOCALHOST

--- src/app/model/Network.ts
+++ src/app/model/Network.ts
@@ -1,5 +1,6 @@
 export class Network {
-    public static API_URL = "http://192.168.10.254:8080/";  
+    //public static API_URL = "http://192.168.10.254:8080/";  
+    public static API_URL = "http://localhost:8080/";  
     //public static API_URL = "http://127.0.0.1:8080/";  
     //public static IMAGE_URL="/src/assets/image";    
     public static IMAGE_URL="assets/image";
